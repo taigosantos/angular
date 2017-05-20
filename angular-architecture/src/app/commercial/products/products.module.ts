6f25c070df674f5e5e7faa627adcbabe078baee8
Tiago Santos <taigobrasil@gmail.com>
2017-05-20 11:06:18 -0300

    criação dos componentes

--- angular-architecture/src/app/commercial/products/products.module.ts
+++ angular-architecture/src/app/commercial/products/products.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductsFormComponent } from './products-form/products-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductsRoutingModule
   ],
-  declarations: []
+  declarations: [ProductsListComponent, ProductsFormComponent]
 })
 export class ProductsModule { }
