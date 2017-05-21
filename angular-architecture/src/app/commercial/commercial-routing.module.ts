51e1c1c4acb9689423ff61741c2d024a32c239fb
Tiago Santos <taigobrasil@gmail.com>
2017-05-21 17:31:25 -0300

    tudo quase 100% só falta arrumar a home

--- angular-architecture/src/app/commercial/commercial-routing.module.ts
+++ angular-architecture/src/app/commercial/commercial-routing.module.ts
@@ -1,9 +1,15 @@
+import { PanelLayoutComponent } from './../core/panel-layout/panel-layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'products', loadChildren: './products/products.module#ProductsModule' }
-];
+const routes: Routes = [{
+  path: 'products',
+  component: PanelLayoutComponent,
+  children: [
+    { path: '', loadChildren: './products/products.module#ProductsModule' }
+  ]
+}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-architecture/src/app/core/core-routing.module.ts
+++ angular-architecture/src/app/core/core-routing.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
+import { PageLayoutComponent } from './page-layout/page-layout.component';
+import { PanelLayoutComponent } from './panel-layout/panel-layout.component';
 
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-  { path: 'page-not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
-  { path: 'internal-error', loadChildren: './internal-error/internal-error.module#InternalErrorModule' },
+const routes: Routes = [{
+  path: 'dashboard',
+  component: PanelLayoutComponent,
+  children: [
+    { path: '', loadChildren: './dashboard/dashboard.module#DashboardModule' }
+  ]
+}, {
+  path: '',
+  component: PageLayoutComponent,
+  children: [
+    { path: 'page-not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
+    { path: 'internal-error', loadChildren: './internal-error/internal-error.module#InternalErrorModule' },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: '**', redirectTo: 'page-not-found', pathMatch: 'full' }
+  ]
+}];
 
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: 'page-not-found', pathMatch: 'full' }
+// { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+// { path: 'page-not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
+// { path: 'internal-error', loadChildren: './internal-error/internal-error.module#InternalErrorModule' },
 
-];
+// { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+// { path: '**', redirectTo: 'page-not-found', pathMatch: 'full' }
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
