e4d7e314f28ab3a4e5c189b59efaf8d14ca2021f
Tiago Santos <taigobrasil@gmail.com>
2017-05-21 17:13:58 -0300

    criação dos componentes de layout

--- angular-architecture/src/app/core/core.module.ts
+++ angular-architecture/src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { PanelLayoutComponent } from './panel-layout/panel-layout.component';
+import { PageLayoutComponent } from './page-layout/page-layout.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule
   ],
-  declarations: []
+  declarations: [PanelLayoutComponent, PageLayoutComponent]
 })
 export class CoreModule { }
---
+++ angular-architecture/src/app/core/panel-layout/panel-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-layout',
+  templateUrl: './panel-layout.component.html',
+  styleUrls: ['./panel-layout.component.scss']
+})
+export class PanelLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
