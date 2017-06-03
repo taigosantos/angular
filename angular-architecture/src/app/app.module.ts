87ba6903b0ad0b776b67422c8ec824e53102f459
Tiago Santos <taigobrasil@gmail.com>
2017-06-03 15:56:27 -0300

    0.5.0

--- angular-architecture/src/app/app.module.ts
+++ angular-architecture/src/app/app.module.ts
@@ -4,25 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-
- // application modules 
+// application modules
 import { CommercialModule } from './commercial/commercial.module';
-import { CoreModule } from "./core/core.module";
+import { CoreModule } from './core/core.module';
+
+// application components
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
     // angular modules
     BrowserModule,
     FormsModule,
     HttpModule,
     RouterModule,
+
     // application modules
     CommercialModule,
     CoreModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-architecture/src/app/core/dashboard/dashboard.module.ts
+++ angular-architecture/src/app/core/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterializeModule } from 'ng2-materialize';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
@@ -7,6 +8,7 @@ import { DashboardPageComponent } from './dashboard-page/dashboard-page.componen
 @NgModule({
   imports: [
     CommonModule,
+    MaterializeModule.forRoot(),
     DashboardRoutingModule
   ],
   declarations: [DashboardPageComponent]
