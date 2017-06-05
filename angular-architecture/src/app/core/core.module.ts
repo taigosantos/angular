9860147bbffb000e4983db08fa863363171b8579
Tiago Santos <taigobrasil@gmail.com>
2017-06-05 01:31:34 -0300

    pd

--- angular-architecture/src/app/core/core.module.ts
+++ angular-architecture/src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterializeModule } from 'ng2-materialize';
+
 import { CoreRoutingModule } from './core-routing.module';
 import { PanelLayoutComponent } from './panel-layout/panel-layout.component';
 import { PageLayoutComponent } from './page-layout/page-layout.component';
 
 @NgModule({
   imports: [
+    MaterializeModule.forRoot(),
     CommonModule,
     CoreRoutingModule
   ],
