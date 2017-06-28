523549e0926d0e3f05a7a8b08d6990ea776264d6
Tiago Santos <tiago.santos@contmatic.com.br>
2017-06-28 10:31:50 -0300

    atualização do formulário de testes

--- angular-architecture/src/app/samples/materialize-support/materialize-forms/materialize-forms.component.ts
+++ angular-architecture/src/app/samples/materialize-support/materialize-forms/materialize-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-materialize-forms',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./materialize-forms.component.scss']
 })
 export class MaterializeFormsComponent implements OnInit {
+
+  testForm: FormGroup;
 
   constructor() { }
 
