a1d5c4469c9fa99cd5d2aac0fec5b73494633352
Tiago Santos <taigobrasil@gmail.com>
2017-06-28 23:54:26 -0300

    udp

--- angular-architecture/src/app/samples/materialize-support/materialize-forms/materialize-forms.component.ts
+++ angular-architecture/src/app/samples/materialize-support/materialize-forms/materialize-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-materialize-forms',
@@ -15,6 +15,10 @@ export class MaterializeFormsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
+let teste = Validators.maxLength(10);
+console.log(teste);
+
     this.testForm = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
       email: [null, [Validators.required, Validators.email]]
@@ -24,13 +28,50 @@ export class MaterializeFormsComponent implements OnInit {
   onSubmit() {
 
     if (!this.testForm.valid)
-      return console.log('Este formulário está inválido');
+      console.log('Este formulário está inválido');
 
-    console.log(this.testForm.value);
+    console.log(this.testForm);
+
   }
 
   onCancel() {
     this.testForm.reset();
+  }
+
+  showMessageErrors(formControlName: string) {
+
+    if (!this.verifyInvalidAndTouchedControl(formControlName))
+      return '';
+
+    const controlErrors: ValidationErrors = this.testForm.get(formControlName).errors;
+    
+    if (controlErrors != null && Object.keys(controlErrors).length > 0) {
+      let key = Object.keys(controlErrors)[0];
+
+      // Object.keys(controlErrors).forEach(keyError => {
+      //   console.log('Key control: ' + formControlName + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+      // });
+    }
+
+
+
+    return '';
+
+  }
+
+  insertErrorStyle(formControlName: string) {
+    return {
+      'invalid': this.verifyInvalidAndTouchedControl(formControlName),
+      'valid': this.verifyValidAndTouchedControl(formControlName)
+    }
+  }
+
+  verifyInvalidAndTouchedControl(formControlName: string) {
+    return this.testForm.get(formControlName).invalid && this.testForm.get(formControlName).touched;
+  }
+
+  verifyValidAndTouchedControl(formControlName: string) {
+    return this.testForm.get(formControlName).valid && this.testForm.get(formControlName).touched;
   }
 
 }
