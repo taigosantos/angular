33955e777c163e98df6406870cde98724da4287c
Tiago Santos <tiago.santos@contmatic.com.br>
2017-07-03 14:09:37 -0300

    suporte ao angular material

--- angular-architecture/src/app/shared/shared.module.ts
+++ angular-architecture/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
 
 import { SamplesLayoutComponent } from './components/samples-layout/samples-layout.component';
 import { PanelLayoutComponent } from './components/panel-layout/panel-layout.component';
@@ -10,25 +11,37 @@ import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule
-  ],
-  declarations: [
-    PageLayoutComponent,
-    PanelLayoutComponent,
-    SamplesLayoutComponent,
-    HeaderComponent
-  ],
-  exports: [
+
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
+    MaterialModule,
+
+  ],
+  declarations: [
+
+    // Layout components
     PageLayoutComponent,
     PanelLayoutComponent,
-    SamplesLayoutComponent
+    SamplesLayoutComponent,
+    HeaderComponent,
+
+  ],
+  exports: [
+
+    // Modules
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MaterialModule,
+
+    // Layouts components
+    PageLayoutComponent,
+    PanelLayoutComponent,
+    SamplesLayoutComponent,
+
   ]
 })
 export class SharedModule { }
