555ef49fa81aab39fee72d9a34c82e188e3e8db8
Tiago Santos <taigobrasil@gmail.com>
2017-07-03 00:21:26 -0300

    udp

--- angular-architecture/src/app/samples/materialize-support/materialize-forms/materialize-forms.component.ts
+++ angular-architecture/src/app/samples/materialize-support/materialize-forms/materialize-forms.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ValidationErrors } from '@angular/forms';
 
+declare var $: any;
+
 @Component({
   selector: 'app-materialize-forms',
   templateUrl: './materialize-forms.component.html',
@@ -23,7 +25,8 @@ export class MaterializeFormsComponent implements OnInit {
   ngOnInit() {
     this.testForm = this.formBuilder.group({
       name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
-      email: [null, [Validators.required, Validators.email]]
+      email: [null, [Validators.required, Validators.email, Validators.maxLength(60)]],
+      genre: [null, Validators.required]
     });
   }
 
@@ -32,20 +35,35 @@ export class MaterializeFormsComponent implements OnInit {
     if (!this.testForm.valid)
       console.log('Este formulário está inválido');
 
+
     console.log(this.testForm);
 
   }
 
   onCancel() {
     this.testForm.reset();
+
+    let a = this.testForm.get('genre');
+
   }
 
   // @ METHODS
+
+  formIsValid(){
+    return this.testForm.valid;
+  }
 
   insertErrorStyle(formControlName: string): Object {
     return {
       'invalid': this.verifyInvalidAndTouchedControl(formControlName),
       'valid': this.verifyValidAndTouchedControl(formControlName)
+    }
+  }
+  
+  insertSelectErrorStyle(formControlName: string): Object {
+    return {
+      'invalid': this.verifyInvalidAndDirtyControl(formControlName),
+      'valid': this.verifyValidAndDirtyControl(formControlName)
     }
   }
 
@@ -55,6 +73,14 @@ export class MaterializeFormsComponent implements OnInit {
 
   verifyValidAndTouchedControl(formControlName: string): boolean {
     return this.testForm.get(formControlName).valid && this.testForm.get(formControlName).touched;
+  }
+
+  verifyInvalidAndDirtyControl(formControlName: string): boolean {
+    return this.testForm.get(formControlName).invalid && this.testForm.get(formControlName).dirty;
+  }
+
+  verifyValidAndDirtyControl(formControlName: string): boolean {
+    return this.testForm.get(formControlName).valid && this.testForm.get(formControlName).dirty;
   }
 
   nextInvalidError(formControlName: string, errorKey: string): boolean {
@@ -73,5 +99,5 @@ export class MaterializeFormsComponent implements OnInit {
   getMaxlengthErrorInfo(formControlName: string): any {
     return this.testForm.get(formControlName).errors['maxlength'];
   }
-  
+
 }
